f9fc2482858e71d7b498b0aa6f9c5baa5dcec459
chaaragSS <network.codeit@gmail.com>
2017-07-01 17:05:23 +0530

    add mustache for temlate rendering

--- playground/time.js
+++ playground/time.js
@@ -1,11 +1,12 @@
   //standard  is Jan 1 1970  00:00:00 am   in utc 
 const moment = require('moment');
-
+/*
 var date = new Date();
 var current=console.log(date.getTime()); //2017-07-01T08:47:57.267Z
 console.log(date.getMonth());  //7
-//var month=['jan','feb',[...]]; 
-console.log(moment().valueOf());
+//var month=['jan','feb',[...]];
+ */
+console.log(moment().valueOf());  //console.log(date.getTime());
 var time = moment();
 console.log(time.format());  //2017-07-01T14:17:57+05:30
 console.log(time.format('MMM'));  // jul
